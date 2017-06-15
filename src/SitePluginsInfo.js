c26b1995f352eb053472224f9471aa8d24df4f19
Tyler Kemme <tpkemme@gmail.com>
2017-06-14 21:38:00 -0500

    fixed issue where spaces in dockerPath were not being escaped

--- src/SitePluginsInfo.js
+++ src/SitePluginsInfo.js
@@ -4,25 +4,25 @@ const path = require('path');
 
 module.exports = function( context ) {
 
-    const Component = context.React.Component
-    const React = context.React
-    const $ = context.jQuery
+  const Component = context.React.Component
+  const React = context.React
+  const $ = context.jQuery
 
-    return class SitePluginsInfo extends Component {
-        constructor( props ) {
-            super( props )
-            // init class vars
-            this.state = {
-	    		activeContent: null,
+  return class SitePluginsInfo extends Component {
+    constructor( props ) {
+      super( props )
+          // init class vars
+      this.state = {
+    		activeContent: null,
 				inactiveContent: null,
 				content: null
-			}
+      }
 
-			this.stylesheetPath = path.resolve(__dirname, '../style.css');
-        }
+      this.stylesheetPath = path.resolve(__dirname, '../style.css');
+    }
 
-        componentDidMount() {
-            // set up
+    componentDidMount() {
+      // set up
 			if ( 'running' === this.props.siteStatus ) {
 				this.getPluginList();
 			} else {
@@ -30,11 +30,11 @@ module.exports = function( context ) {
 				this.setState( { activeContent: null} )
 				this.setState( { inactiveContent: null} )
 			}
-        }
+    }
 
-        componentWillUnmount() {
-            // tear down
-        }
+    componentWillUnmount() {
+        // tear down
+    }
 
 		getPluginList() {
 
@@ -54,126 +54,127 @@ module.exports = function( context ) {
 					<td className="plugins-table-info"> - </td>
 				</tr> } )
 
-		    // get site object using siteID
-		    let site = this.props.sites[ this.props.params.siteID ]
+	    // get site object using siteID
+	    let site = this.props.sites[ this.props.params.siteID ]
 
-		    // construct command using bundled docker binary to execute 'wp plugin list' inside container for active plugins
-		    let activeCommand = `${context.environment.dockerPath} exec ${site.container} wp plugin list --status=active --path=/app/public --format=csv --allow-root`
+	    // construct command using bundled docker binary to execute 'wp plugin list' inside container for active plugins
+      // IMPORTANT can't forget to escape any spaces in dockerPath!
+      let activeCommand = `${ context.environment.dockerPath.replace(/ /g, "\\ ") } exec ${site.container} wp plugin list --status=active --path=/app/public --format=csv --allow-root`
+      
+	    // execute command in docker env and run callback when it returns
+	    childProcess.exec( activeCommand, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
+	        // Display error message if there's an issue
+	        if (error) {
+            this.setState( { activeContent:
+  					<tr>
+  						<td className="plugins-table-name">Error retrieving active plugin: <pre>{ error, stdout, stderr }</pre></td>
+  						<td className="plugins-table-info"> - </td>
+  						<td className="plugins-table-info"> - </td>
+  					</tr> } )
+	        } 
+          else {
+	            // split list into array
+            let plugins = stdout.trim().split( "\n" )
+	          plugins.splice(0, 1)
 
-		    // execute command in docker env and run callback when it returns
-		    childProcess.exec( activeCommand, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
-		        // Display error message if there's an issue
-		        if (error) {
-		            this.setState( { activeContent:
-						<tr>
-							<td className="plugins-table-name">Error retrieving active plugin: <pre>{ stderr }</pre></td>
-							<td className="plugins-table-info"> - </td>
-							<td className="plugins-table-info"> - </td>
-						</tr> } )
-		        } else {
-		            // split list into array
-		            let plugins = stdout.trim().split( "\n" )
-					plugins.splice(0, 1)
-
-		            // Only create unordered list if there are plugins to list
-		            if ( plugins.length && plugins[0].length > 1 ) {
-		                this.setState( { activeContent: plugins.map( (item) =>
-							<tr>
-								<td className="plugins-table-name" key={ plugins.indexOf(item) }>{ item.trim().split( "," )[0] }</td>
-								<td className={ item.trim().split( "," )[2] + " plugins-table-info" } >{ item.trim().split( "," )[2] }</td>
-								<td className="plugins-table-info">{ item.trim().split( "," )[3] }</td>
-							</tr> ) } )
-		            } else {
-		                this.setState( { activeContent:
-							<tr>
-								<td className="plugins-table-name">No active plugins.</td>
-								<td className="plugins-table-info"> - </td>
-								<td className="plugins-table-info"> - </td>
-							</tr> } )
-		            }
-		        }
-		    } );
+	            // Only create unordered list if there are plugins to list
+            if ( plugins.length && plugins[0].length > 1 ) {
+              this.setState( { activeContent: plugins.map( (item) =>
+  						<tr>
+  							<td className="plugins-table-name" key={ plugins.indexOf(item) }>{ item.trim().split( "," )[0] }</td>
+  							<td className={ item.trim().split( "," )[2] + " plugins-table-info" } >{ item.trim().split( "," )[2] }</td>
+  							<td className="plugins-table-info">{ item.trim().split( "," )[3] }</td>
+  						</tr> ) } )
+            } 
+            else {
+              this.setState( { activeContent:
+  						<tr>
+  							<td className="plugins-table-name">No active plugins.</td>
+  							<td className="plugins-table-info"> - </td>
+  							<td className="plugins-table-info"> - </td>
+  						</tr> } )
+            }
+	        }
+	    } );
 
 			// construct command using bundled docker binary to execute 'wp plugin list' inside container for inactive plugins
-		    let inactiveCommand = `${context.environment.dockerPath} exec ${site.container} wp plugin list --status=inactive --path=/app/public --format=csv --allow-root`
+      // IMPORTANT can't forget to escape any spaces in dockerPath!
+      let inactiveCommand = `${ context.environment.dockerPath.replace(/ /g, "\\ ") } exec ${site.container} wp plugin list --status=inactive --path=/app/public --format=csv --allow-root`
 
-		    // execute command in docker env and run callback when it returns
-		    childProcess.exec( inactiveCommand, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
-		        // Display error message if there's an issue
-		        if (error) {
-		            this.setState( { inactiveContent:
+	    // execute command in docker env and run callback when it returns
+	    childProcess.exec( inactiveCommand, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
+	        // Display error message if there's an issue
+	        if (error) {
+            this.setState( { inactiveContent:
 						<tr>
 							<td className="plugins-table-name">Error retrieving inactive plugin: <pre>{ stderr }</pre></td>
 							<td className="plugins-table-info"> - </td>
 							<td className="plugins-table-info"> - </td>
 						</tr> } )
-		        } else {
-		            // split list into array
-		            let plugins = stdout.trim().split( "\n" )
-					plugins.splice(0, 1)
+	        } 
+          else {
+            // split list into array
+            let plugins = stdout.trim().split( "\n" )
+            plugins.splice(0, 1)
 
-		            // Only create unordered list if there are plugins to list
-		            if ( plugins.length && plugins[0].length > 1 ) {
-		                this.setState( { inactiveContent: plugins.map( (item) =>
+            // Only create unordered list if there are plugins to list
+            if ( plugins.length && plugins[0].length > 1 ) {
+              this.setState( { inactiveContent: plugins.map( (item) =>
 							<tr>
 								<td className="plugins-table-name" key={ plugins.indexOf(item) }>{ item.trim().split( "," )[0] }</td>
 								<td className={ item.trim().split( "," )[2] + " plugins-table-info" } >{ item.trim().split( "," )[2] }</td>
 								<td className="plugins-table-info">{ item.trim().split( "," )[3] }</td>
 							</tr> ) } )
-		            } else {
-		                this.setState( { inactiveContent:
-							<tr>
-								<td className="plugins-table-name">No inactive plugins.</td>
-								<td className="plugins-table-info"> - </td>
-								<td className="plugins-table-info"> - </td>
-							</tr> } )
-		            }
-		        }
-		    } );
+            } else {
+                    this.setState( { inactiveContent:
+            	<tr>
+            		<td className="plugins-table-name">No inactive plugins.</td>
+            		<td className="plugins-table-info"> - </td>
+            		<td className="plugins-table-info"> - </td>
+            	</tr> } )
+            }
+	        }
+	    } );
 
 			this.setState( { content: null} )
-
 		}
 
-        render() {
-            return (
+    render() {
+      return (
 				<div className="plugins-container">
 					<link rel="stylesheet" href={this.stylesheetPath}/>
 
 					{ this.state.content }
 
-                    <h3 className="plugins-table-title">Active Plugins</h3>
+          <h3 className="plugins-table-title">Active Plugins</h3>
 					<table className="table-striped plugins-table">
 						<thead>
-						<tr className="plugin-table-head" >
-							<th className="plugins-table-name"><strong>Name</strong></th>
-							<th className="plugins-table-info"><strong>Update</strong></th>
-							<th className="plugins-table-info"><strong>Version</strong></th>
-						</tr>
+  						<tr className="plugin-table-head" >
+  							<th className="plugins-table-name"><strong>Name</strong></th>
+  							<th className="plugins-table-info"><strong>Update</strong></th>
+  							<th className="plugins-table-info"><strong>Version</strong></th>
+  						</tr>
 						</thead>
 						<tbody>
-
-						{ this.state.activeContent }
-
+              { this.state.activeContent }
 						</tbody>
 					</table>
 
-                    <h3 className="plugins-table-title">Inactive Plugins</h3>
+          <h3 className="plugins-table-title">Inactive Plugins</h3>
 					<table className="table-striped plugins-table">
 						<thead>
-						<tr className="plugin-table-head" >
-							<th className="plugins-table-name"><strong>Name</strong></th>
-							<th className="plugins-table-info"><strong>Update</strong></th>
-							<th className="plugins-table-info"><strong>Version</strong></th>
-						</tr>
+  						<tr className="plugin-table-head" >
+  							<th className="plugins-table-name"><strong>Name</strong></th>
+  							<th className="plugins-table-info"><strong>Update</strong></th>
+  							<th className="plugins-table-info"><strong>Version</strong></th>
+  						</tr>
 						</thead>
 						<tbody>
-						{ this.state.inactiveContent }
+              { this.state.inactiveContent }
 						</tbody>
 					</table>
-                </div>
-            );
-        }
+        </div>
+      );
     }
-
+  }
 }
--- src/SiteThemesInfo.js
+++ src/SiteThemesInfo.js
@@ -4,25 +4,25 @@ const path = require('path');
 
 module.exports = function( context ) {
 
-    const Component = context.React.Component
-    const React = context.React
-    const $ = context.jQuery
+  const Component = context.React.Component
+  const React = context.React
+  const $ = context.jQuery
 
-    return class SiteThemesInfo extends Component {
-        constructor( props ) {
-            super( props )
-            // init class vars
-            this.state = {
-	    		activeContent: null,
+  return class SiteThemesInfo extends Component {
+    constructor( props ) {
+      super( props )
+      // init class vars
+      this.state = {
+    		activeContent: null,
 				inactiveContent: null,
 				content: null
 			}
 
 			this.stylesheetPath = path.resolve(__dirname, '../style.css');
-        }
+    }
 
-        componentDidMount() {
-            // set up
+    componentDidMount() {
+      // set up
 			if ( 'running' === this.props.siteStatus ) {
 				this.getThemeList();
 			} else {
@@ -30,11 +30,11 @@ module.exports = function( context ) {
 				this.setState( { activeContent: null} )
 				this.setState( { inactiveContent: null} )
 			}
-        }
+    }
 
-        componentWillUnmount() {
-            // tear down
-        }
+    componentWillUnmount() {
+        // tear down
+    }
 
 		getThemeList() {
 
@@ -54,126 +54,126 @@ module.exports = function( context ) {
 					<td className="themes-table-info"> - </td>
 				</tr> } )
 
-		    // get site object using siteID
-		    let site = this.props.sites[ this.props.params.siteID ]
+	    // get site object using siteID
+	    let site = this.props.sites[ this.props.params.siteID ]
 
-		    // construct command using bundled docker binary to execute 'wp theme list' inside container for active themes
-		    let activeCommand = `${context.environment.dockerPath} exec ${site.container} wp theme list --status=active --path=/app/public --format=csv --allow-root`
+	    // construct command using bundled docker binary to execute 'wp theme list' inside container for active themes
+	    // IMPORTANT can't forget to escape any spaces in dockerPath!
+	    let activeCommand = `${ context.environment.dockerPath.replace(/ /g, "\\ ") } exec ${site.container} wp theme list --status=active --path=/app/public --format=csv --allow-root`
 
-		    // execute command in docker env and run callback when it returns
-		    childProcess.exec( activeCommand, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
-		        // Display error message if there's an issue
-		        if (error) {
-		            this.setState( { activeContent:
-						<tr>
-							<td className="themes-table-name">Error retrieving active theme: <pre>{ stderr }</pre></td>
-							<td className="themes-table-info"> - </td>
-							<td className="themes-table-info"> - </td>
-						</tr> } )
-		        } else {
-		            // split list into array
-		            let themes = stdout.trim().split( "\n" )
-					themes.splice(0, 1)
+	    // execute command in docker env and run callback when it returns
+	    childProcess.exec( activeCommand, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
+        // Display error message if there's an issue
+        if (error) {
+            this.setState( { activeContent:
+				<tr>
+					<td className="themes-table-name">Error retrieving active theme: <pre>{ stderr }</pre></td>
+					<td className="themes-table-info"> - </td>
+					<td className="themes-table-info"> - </td>
+				</tr> } )
+        } else {
+          // split list into array
+          let themes = stdout.trim().split( "\n" )
+		      themes.splice(0, 1)
 
-		            // Only create unordered list if there are themes to list
-		            if ( themes.length && themes[0].length > 1 ) {
-		                this.setState( { activeContent: themes.map( (item) =>
-							<tr>
-								<td className="themes-table-name" key={ themes.indexOf(item) }>{ item.trim().split( "," )[0] }</td>
-								<td className={ item.trim().split( "," )[2] + " themes-table-info" } >{ item.trim().split( "," )[2] }</td>
-								<td className="themes-table-info">{ item.trim().split( "," )[3] }</td>
-							</tr> ) } )
-		            } else {
-		                this.setState( { activeContent:
-							<tr>
-								<td className="themes-table-name">No active themes.</td>
-								<td className="themes-table-info"> - </td>
-								<td className="themes-table-info"> - </td>
-							</tr> } )
-		            }
-		        }
-		    } );
+          // Only create unordered list if there are themes to list
+          if ( themes.length && themes[0].length > 1 ) {
+            this.setState( { activeContent: themes.map( (item) =>
+  						<tr>
+  							<td className="themes-table-name" key={ themes.indexOf(item) }>{ item.trim().split( "," )[0] }</td>
+  							<td className={ item.trim().split( "," )[2] + " themes-table-info" } >{ item.trim().split( "," )[2] }</td>
+  							<td className="themes-table-info">{ item.trim().split( "," )[3] }</td>
+  						</tr> ) } )
+          } else {
+            this.setState( { activeContent:
+  						<tr>
+  							<td className="themes-table-name">No active themes.</td>
+  							<td className="themes-table-info"> - </td>
+  							<td className="themes-table-info"> - </td>
+  						</tr> } )
+            }
+	        }
+	    } );
 
 			// construct command using bundled docker binary to execute 'wp theme list' inside container for inactive themes
-		    let inactiveCommand = `${context.environment.dockerPath} exec ${site.container} wp theme list --status=inactive --path=/app/public --format=csv --allow-root`
+      // IMPORTANT can't forget to escape any spaces in dockerPath!
+      let inactiveCommand = `${ context.environment.dockerPath.replace(/ /g, "\\ ") } exec ${site.container} wp theme list --status=inactive --path=/app/public --format=csv --allow-root`
 
-		    // execute command in docker env and run callback when it returns
-		    childProcess.exec( inactiveCommand, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
-		        // Display error message if there's an issue
-		        if (error) {
-		            this.setState( { inactiveContent:
-						<tr>
-							<td className="themes-table-name">Error retrieving inactive theme: <pre>{ stderr }</pre></td>
-							<td className="themes-table-info"> - </td>
-							<td className="themes-table-info"> - </td>
-						</tr> } )
-		        } else {
-		            // split list into array
-		            let themes = stdout.trim().split( "\n" )
-					themes.splice(0, 1)
+	    // execute command in docker env and run callback when it returns
+	    childProcess.exec( inactiveCommand, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
+        // Display error message if there's an issue
+        if (error) {
+          this.setState( { inactiveContent:
+  					<tr>
+  						<td className="themes-table-name">Error retrieving inactive theme: <pre>{ stderr }</pre></td>
+  						<td className="themes-table-info"> - </td>
+  						<td className="themes-table-info"> - </td>
+  					</tr> } )
+        } else {
+          // split list into array
+          let themes = stdout.trim().split( "\n" )
+  				themes.splice(0, 1)
 
-		            // Only create unordered list if there are themes to list
-		            if ( themes.length && themes[0].length > 1 ) {
-		                this.setState( { inactiveContent: themes.map( (item) =>
-							<tr>
-								<td className="themes-table-name" key={ themes.indexOf(item) }>{ item.trim().split( "," )[0] }</td>
-								<td className={ item.trim().split( "," )[2] + " themes-table-info" } >{ item.trim().split( "," )[2] }</td>
-								<td className="themes-table-info">{ item.trim().split( "," )[3] }</td>
-							</tr> ) } )
-		            } else {
-		                this.setState( { inactiveContent:
-							<tr>
-								<td className="themes-table-name">No inactive themes.</td>
-								<td className="themes-table-info"> - </td>
-								<td className="themes-table-info"> - </td>
-							</tr> } )
-		            }
-		        }
-		    } );
+          // Only create unordered list if there are themes to list
+          if ( themes.length && themes[0].length > 1 ) {
+            this.setState( { inactiveContent: themes.map( (item) =>
+  						<tr>
+  							<td className="themes-table-name" key={ themes.indexOf(item) }>{ item.trim().split( "," )[0] }</td>
+  							<td className={ item.trim().split( "," )[2] + " themes-table-info" } >{ item.trim().split( "," )[2] }</td>
+  							<td className="themes-table-info">{ item.trim().split( "," )[3] }</td>
+  						</tr> ) } 
+            )
+          } else {
+            this.setState( { inactiveContent:
+  						<tr>
+  							<td className="themes-table-name">No inactive themes.</td>
+  							<td className="themes-table-info"> - </td>
+  							<td className="themes-table-info"> - </td>
+  						</tr> } )
+            }
+	        }
+	    } );
 
-			this.setState( { content: null} )
+		this.setState( { content: null} )
 
 		}
 
-        render() {
-            return (
+    render() {
+      return (
 				<div className="themes-container">
 					<link rel="stylesheet" href={this.stylesheetPath}/>
 
 					{ this.state.content }
 
-                    <h3 className="themes-table-title">Active Theme</h3>
+          <h3 className="themes-table-title">Active Theme</h3>
 					<table className="table-striped themes-table">
 						<thead>
-						<tr className="theme-table-head" >
-							<th className="themes-table-name"><strong>Name</strong></th>
-							<th className="themes-table-info"><strong>Update</strong></th>
-							<th className="themes-table-info"><strong>Version</strong></th>
-						</tr>
+    					<tr className="theme-table-head" >
+    						<th className="themes-table-name"><strong>Name</strong></th>
+    						<th className="themes-table-info"><strong>Update</strong></th>
+    						<th className="themes-table-info"><strong>Version</strong></th>
+    					</tr>
 						</thead>
-						<tbody>
-
-						{ this.state.activeContent }
-
+            <tbody>
+				      { this.state.activeContent }
 						</tbody>
 					</table>
 
-                    <h3 className="themes-table-title">Inactive Themes</h3>
+          <h3 className="themes-table-title">Inactive Themes</h3>
 					<table className="table-striped themes-table">
 						<thead>
-						<tr className="theme-table-head" >
-							<th className="themes-table-name"><strong>Name</strong></th>
-							<th className="themes-table-info"><strong>Update</strong></th>
-							<th className="themes-table-info"><strong>Version</strong></th>
-						</tr>
+  						<tr className="theme-table-head" >
+  							<th className="themes-table-name"><strong>Name</strong></th>
+  							<th className="themes-table-info"><strong>Update</strong></th>
+  							<th className="themes-table-info"><strong>Version</strong></th>
+  						</tr>
 						</thead>
 						<tbody>
-						{ this.state.inactiveContent }
+				      { this.state.inactiveContent }
 						</tbody>
 					</table>
-                </div>
-            );
-        }
+        </div>
+      );
     }
-
+  }
 }
